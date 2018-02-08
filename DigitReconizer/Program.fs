bc8f4ea398961631717b9028f828408685baf6b9
Malte Neve-Græsbøll <malte.neve-graesboell@keylane.com>
2018-02-08 19:42:56 +0100

    * .gitignore: Initital Chapter 1
    * MachineLearning.sln:
    * Tutorial.fs:
    * Library.fs:
    * Program.fs:
    * packages.config:
    * F_Tutorial.fsproj:
    * AssemblyInfo.fs:
    * packages.config:
    * DigitReconizer.fsproj:

---
+++ DigitReconizer/Program.fs
@@ -0,0 +1,17 @@
+﻿open System.IO
+
+type Observation = { Label:string; Pixels:int[] }
+
+let toObservation (csvData:string) =
+    let colums = csvData.Split(',')
+    let label = colums.[0]
+    let pixels = colums.[1..] |> Array.map int
+    { Label = label; Pixels = pixels}
+
+let reader path =
+    let data = File.ReadAllLines path
+    data.[1..]
+    |> Array.map toObservation
+
+let trainingPath = @"/Users/malte/Documents/Schantz/MachineLearning/trainingsample.csv"
+let trainingData = reader trainingPath
